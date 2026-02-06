a2be5ba332c9295b65bede28ff02fc67978cd9b5
Vikas K Singla <vikas.k.singla@mongodb.com>
2026-02-06 15:40:38 +0530

    Update MongoDB Atlas cluster configuration for HyderabadZone and GCP region

--- main.tf
+++ main.tf
@@ -9,12 +9,35 @@ resource "mongodbatlas_advanced_cluster" "global_cluster" {
       region_configs = [
         {
           electable_specs = {
-            instance_size = "M30"
-            node_count    = 3
+            instance_size = "M40"
+            node_count    = 2
+            autoscaling = {
+              compute = {
+                enabled            = true
+                min_instance_size = "M30"
+                max_instance_size = "M40"
+              }
+            }
           }
           provider_name = "AWS"
           priority      = 7
           region_name   = "AP_SOUTH_1"
+        },
+        {
+          electable_specs = {
+            instance_size = "M40"
+            node_count    = 1
+            autoscaling = {
+              compute = {
+                enabled            = true
+                min_instance_size = "M30"
+                max_instance_size = "M40"
+              }
+            }
+          }
+          provider_name = "GCP"
+          priority      = 6
+          region_name   = "EASTERN_US"
         }
       ]
     },
@@ -29,7 +52,7 @@ resource "mongodbatlas_advanced_cluster" "global_cluster" {
           provider_name = "AWS"
           priority      = 7
           region_name   = "AP_SOUTH_2"
-        }
+        },
       ]
     }
   ]
